627590b8ee7a7a3c252c937f8e15f68c3f25660b
Peter Chen <747853229@qq.com>
2017-09-18 22:12:31 +0800

    Version 0.6

---
+++ data/post-data.js
@@ -0,0 +1,26 @@
+let post_contents = [
+    {
+        date: "Nov 25 2016",
+        title: "比利·林恩的中场故事",
+        imgSrc: "/images/post/bl.png",
+        avatar: "李安是以为绝不会重复自己的导演",
+        reading: "112",
+        collection: "96",
+        avatar: "/images/avatar/2.png",
+        postId: "0"
+    },
+    {
+        date: "Nov 25 2016",
+        title: "比利·林恩的中场故事",
+        imgSrc: "/images/post/bl.png",
+        avatar: "李安是以为绝不会重复自己的导演",
+        reading: "112",
+        collection: "96",
+        avatar: "/images/avatar/2.png",
+        postId: "1"
+    }
+]
+
+module.exports = {
+    postList:post_contents
+}
